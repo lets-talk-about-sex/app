131d9085ac99aeb939b78687dcc7eba0f8515c86
hugrunrunars <hugrunrunars95@gmail.com>
2019-04-26 16:30:03 +0000

    laga conflict

--- src/components/theme/theme.js
+++ src/components/theme/theme.js
@@ -2,11 +2,7 @@ export const theme = {
     baseColors: {
       bodyBackground:'#f9f9f9;',  
       coral: '#fc4255;',
-<<<<<<< HEAD
-=======
       black: '#000;',
       searchText: '#c1c1c1;',
-
->>>>>>> 4e04408cdf12af6448755c6314c64de89991e00a
     },
   };
