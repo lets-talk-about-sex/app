8829d46472786e61f979531166606b913c268e3a
Anna Sóley <annasoley09@gmail.com>
2019-04-30 10:21:49 +0000

    laga base css

--- src/components/base/base.js
+++ src/components/base/base.js
@@ -1,65 +1,73 @@
-
 import React from 'react';
 import { injectGlobal } from "emotion";
 import { withTheme } from "emotion-theming";
 
 class Global extends React.Component {
-    render() {
-      injectGlobal`
-      * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
+   render() {
+     injectGlobal`
+     * {
+       box-sizing: border-box;
+       margin: 0 20px;
+       padding: 0;
+   }
+      /* Fontur */
+      @import url('https://fonts.googleapis.com/css?family=PT+Sans%7CPoppins:400,600,700,800');
 
-       @import url('https://fonts.googleapis.com/css?family=PT+Sans|Poppins:400,600,700');
+      body {
+       background-color: #f9f9f9;
+      }
 
-       body {
-  
-       }
+      h1, h2, h3, h4, h5 {
+          font-family: 'Poppins', sans-serif;
+          color: #000;
+      }
 
-       h1, h2, h3 {
-           font-family: 'Poppins', sans-serif;
-           color: #000;
-       }
-       /* Titill á undirsíðu með stöku orði */
-       h1 {
-           font-size: 32px; 
+      /* Titill á undirsíðu með stöku orði */
+      h1 {
+          font-size: 32px;
+          font-weight: 600;
+      }
+
+      /* Flokkar */
+      h2 {
+       font-size: 26px;
+       font-weight: 600;
+      }
+
+      /* Titlar á spjöldum */
+      h2 {
+          font-size: 20px;
+          font-weight: 600;
+      }
+
+      h3 {
+          font-size: 18px;
+          font-weight: 600;
+      }
+
+       /* Small card */
+       h4 {
+           font-size: 16px;
            font-weight: 600;
        }
 
-       /* Flokkar */
-       h2 {
-        font-size: 26px;
-        font-weight: 600;
+         /* Heading info card */
+       h5 {
+         font-size: 16px;
+         font-weight: 600;
+         color: #FC4255;
        }
 
-       /* Titlar á spjöldum */
-       h2 {
-           font-size: 22px;
-           font-weight: 600;
+       h6 {
+         font-size: 14px;
+         text-transform: uppercase;
+         color: #FC4255;
        }
-   
-       h3 {
-           font-size: 26px;  
-       }
-
-         /* Undirtitlar á undirsíðu */
-        h4 {
-            font-size: 18px;  
-       }
-
-          /* Slug á spjaldi */
-          h5 {
-            font-size: 14px;
-            font-weight: 600;
-            color: #FC4255;
-       }
-
 
        p {
-            font-family: 'PT Sans', sans-serif;
-            font-size: 16px; 
+         font-family: 'PT Sans', sans-serif;
+         font-size: 16px;
+         color: #000;
        }
 
        li {
@@ -67,26 +75,16 @@ class Global extends React.Component {
        }
 
        a {
-          text-decoration:none;
-          color:#C7C7C7;
-            &:visited {
-             color:#C7C7C7;
-            }
+         text-decoration:none;
+         color:#C7C7C7;
+           &:visited{
+           color:#C7C7C7;
+           }
        }
 
-       input {
-         font-family: 'Poppins', sans-serif;
-       }
+     `;
+     return React.Children.only(this.props.children);
+   }
+ }
 
- 
-
-      `;
-      return React.Children.only(this.props.children);
-    }
-  }
-  
-  export default withTheme(Global);
-
-
-
-
+ export default withTheme(Global);
