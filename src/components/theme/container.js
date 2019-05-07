6f3af08ac7a91170accb46a0d55ead017f6d40b4
birgittarun92 <birgittarun92@gmail.com>
2019-05-07 16:34:30 +0000

    PWA og laga loading og flr

--- src/components/theme/container.js
+++ src/components/theme/container.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled/macro';
 
 const Container = styled.div`
    width: 100vw;
-   margin: 0 30px;
+   padding: 0 30px;
    background-color: ${props => props.theme.baseColors.bodyBackground}
 `
 
